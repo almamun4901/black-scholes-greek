19674088d695a677467f6628b37709faabf248a4
Md Al Mamun <malm@default-10-231-130-164.vpnuser.dartmouth.edu>
2026-03-12 21:48:26 -0400

    fix streamlit

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,10 @@
 # streamlit dashboard
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
